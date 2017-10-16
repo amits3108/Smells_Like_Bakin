569c30fe2f0693ec00e79f526198d18982160a29
Keinix <zackcosborn@gmail.com>
2017-10-16 12:21:32 +0900

    Recycler view now functional

--- app/src/main/java/com/keinix/smellslikebakin/ListFragment.java
+++ app/src/main/java/com/keinix/smellslikebakin/ListFragment.java
@@ -3,6 +3,8 @@ package com.keinix.smellslikebakin;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +16,14 @@ public class ListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         // auto added to container viewGroup. need 3rd arg false to stop double add
         View view = inflater.inflate(R.layout.fragment_list, container, false);
+
+        RecyclerView recyclerView = view.findViewById(R.id.listRecyclerView);
+        ListAdapter listAdapter = new ListAdapter();
+        recyclerView.setAdapter(listAdapter);
+
+        // fragments always have access to their activity vio getActivity()
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
         return view;
     }
 }
