0d0ad079ef51e24d2cb7a2b443df24eae351d8fc
Keinix <zackcosborn@gmail.com>
2017-10-17 23:21:13 +0900

    ViewPagerFragment in place

--- app/src/main/java/com/keinix/smellslikebakin/MainActivity.java
+++ app/src/main/java/com/keinix/smellslikebakin/MainActivity.java
@@ -9,25 +9,37 @@ import android.widget.Toast;
 import static android.R.attr.fragment;
 
 public class MainActivity extends AppCompatActivity implements ListFragment.OnRecipeSelectedInterface {
+    public static final String LIST_FRAGMENT = "list_fragment";
+    public static final String VIEW_PAGER_FRAGMENT = "view_pager_fragment";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListFragment savedFragment = (ListFragment) getFragmentManager().findFragmentById(R.id.placeHolder);
+        ListFragment savedFragment = (ListFragment) getFragmentManager().findFragmentByTag(LIST_FRAGMENT);
 
         if (savedFragment == null) {
             ListFragment fragment = new ListFragment();
             FragmentManager fragmentManager = getFragmentManager();
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            fragmentTransaction.add(R.id.placeHolder, fragment);
+            fragmentTransaction.add(R.id.placeHolder, fragment, LIST_FRAGMENT);
             fragmentTransaction.commit();
         }
     }
 
     @Override
     public void onListRecipeSelected(int index) {
-        Toast.makeText(this, Recipes.names[index], Toast.LENGTH_SHORT).show();
+        ViewPagerFragment fragment = new ViewPagerFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt(ViewPagerFragment.KEY_RECIPE_INDEX, index);
+        fragment.setArguments(bundle);
+
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.placeHolder, fragment, VIEW_PAGER_FRAGMENT);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
     }
 }
--- app/src/main/java/com/keinix/smellslikebakin/ViewPagerFragment.java
+++ app/src/main/java/com/keinix/smellslikebakin/ViewPagerFragment.java
@@ -1,7 +1,30 @@
 package com.keinix.smellslikebakin;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
 public class ViewPagerFragment extends Fragment {
+    public static final String KEY_RECIPE_INDEX = "recipe_index";
 
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_viewpager, container, false);
+
+        int index = getArguments().getInt(KEY_RECIPE_INDEX);
+        getActivity().setTitle(Recipes.names[index]);
+        return view;
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        getActivity().setTitle(getResources().getString(R.string.app_name));
+    }
 }
